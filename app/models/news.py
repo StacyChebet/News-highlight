c180ba1e234d8bb6b1c74fceb6cc28801a95dd52
StacyChebet <staceychebet23@gmail.com>
2018-11-09 16:26:34 +0300

    add the class Headlines with the parameters in the object

---
+++ app/models/news.py
@@ -0,0 +1,12 @@
+class Headlines:
+    '''
+    Defines new News objects
+    '''
+    def __init__(self, author, title, description, url, urlToImage, publishedAt, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt 
+        self.content = content
