e3ea0f68d37c176649eeabc4954905a375e894d2
StacyChebet <staceychebet23@gmail.com>
2018-11-09 12:00:41 +0300

    import flask and app

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+# #Views
+# @app.route ('/')
+# def index():
+#     '''
+#     Returns index page and its data
+#     '''
+#     return render_template('index.html')
