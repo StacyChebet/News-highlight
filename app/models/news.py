b56ecf9e0eb67e4997b3664bd02ebec358bb447d
StacyChebet <staceychebet23@gmail.com>
2018-11-09 16:48:38 +0300

    add the class Sources with the parameters in the object

--- app/models/news.py
+++ app/models/news.py
@@ -10,3 +10,17 @@ class Headlines:
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt 
         self.content = content
+
+class Sources:
+    '''
+    Defines new Sources objects
+    '''
+
+    def __init__(self, source_id, name, description, url, category, language, country):
+        self.source_id = source_id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
